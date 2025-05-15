d3ffee842156bc5a51d2778dc6e1e6e0d49397e6
Tobias Fischer <info@tobiasfischer.info>
2025-05-16 08:34:59 +1000

    Check for conda files on Windows, too

--- .scripts/build_win.bat
+++ .scripts/build_win.bat
@@ -25,5 +25,14 @@ for %%X in (%CURRENT_RECIPES%) do (
     rem -m %FEEDSTOCK_ROOT%\.ci_support\conda_forge_pinnings.yaml
 )
 
-pixi run upload "%CONDA_BLD_PATH%\win-64\*.conda" --force
-if errorlevel 1 exit 1
+:: Check if .conda files exist in the win-64 directory
+dir /b "%CONDA_BLD_PATH%\win-64\*.conda" >nul 2>&1
+if errorlevel 1 (
+    :: No files found, display warning
+    echo Warning: No .conda files found in %CONDA_BLD_PATH%\win-64
+    echo This might be due to all the packages being skipped
+) else (
+    :: Files found, run the upload command
+    pixi run upload "%CONDA_BLD_PATH%\win-64\*.conda" --force
+    if errorlevel 1 exit 1
+)
